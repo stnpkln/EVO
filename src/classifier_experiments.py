91ad84aa2d035a83e9bf7d6014d7c92619249b88
Petr Bromnik <p.bromnik@seznam.cz>
2025-05-02 12:28:25 +0200

    @ logging fix

--- src/classifier_experiments.py
+++ src/classifier_experiments.py
@@ -6,7 +6,7 @@ from multiprocessing import Process
 def evolve_bin_classifiers(batch_size=8, batch_count=4, noises=noise_types):
     for noise_type in noises:
         for i in range(batch_count):
-            print(f"Starting batch {i+1}/{batch_size}, {noise_type}")
+            print(f"Starting batch {i+1}/{batch_count}, {noise_type}")
             runs = []
             for i in range(batch_size):
                 seed = get_new_seed()
