89da69cb52c2393d8fbc17d16a9b60dff47c2e16
Bromnik Petr <xbromn00@merlin.fit.vutbr.cz>
2025-05-04 21:32:27 +0200

    + new evolved classifiers

--- src/classifier_experiments.py
+++ src/classifier_experiments.py
@@ -6,14 +6,14 @@ from multiprocessing import Process
 def evolve_bin_classifiers(batch_size=8, batch_count=4, noises=noise_types):
     for noise_type in noises:
         for i in range(batch_count):
-            print(f"Starting batch {i+1}/{batch_size}, {noise_type}")
+            print(f"Starting batch {i+1}/{batch_count}, {noise_type}")
             runs = []
             for i in range(batch_size):
                 seed = get_new_seed()
-                p = Process(target=evolve_binary_classifier, args=("vertical_noise", window_3x3, seed,))
+                p = Process(target=evolve_binary_classifier, args=(noise_type, window_3x3, seed,))
                 p.start()
                 runs.append(p)
             for p in runs:
                 p.join()
 
-evolve_bin_classifiers(batch_size=6, batch_count=2 , noises=["vertical_noise"])
+evolve_bin_classifiers(batch_size=8, batch_count=3 , noises=[noise_types[1], noise_types[2]])
